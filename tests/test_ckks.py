03ab2e6de4d9dd4c139ef51c4b3812399b0b06d1
youben11 <ayouben9@gmail.com>
2020-07-20 10:23:10 +0100

    tests ckks enc/dec

---
+++ tests/test_ckks.py
@@ -0,0 +1,18 @@
+from he_sim.ckks import CKKS
+import pytest
+
+
+@pytest.mark.parametrize("data", [[1, 2, 3], [0, 0, 0, 0], [-1, 5, 93.5]])
+@pytest.mark.parametrize("poly_mod_degree", [2048, 4096, 8192])
+@pytest.mark.parametrize("scale", [2 ** 20, 2 ** 40])
+@pytest.mark.parametrize("replicated", [True, False])
+def test_encryption_decryption(data, poly_mod_degree, scale, replicated):
+    ct = CKKS(data, poly_mod_degree, scale, replicated)
+    slots = poly_mod_degree // 2
+
+    if replicated:
+        expected_data = (data * (slots // len(data) + 1))[:slots]
+    else:
+        expected_data = data + [0] * (slots - len(data))
+
+    assert ct.decrypt() == expected_data
